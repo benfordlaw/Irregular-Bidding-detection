a37ce0ffca0577d88b7f76353405618f82d18aa4
benfordlaw <77871868+benfordlaw@users.noreply.github.com>
2021-01-25 14:09:20 -0500

    Add files via upload

---
+++ Paper (figures and codes)/Requirment-checking.py
@@ -0,0 +1,56 @@
+#!/usr/bin/env python
+# coding: utf-8
+
+# In[1]:
+
+
+import pandas as pd
+
+
+# In[2]:
+
+
+df= pd.read_csv('NJ_all-csv (2).csv')
+
+
+# In[3]:
+
+
+dff= df[['Extension']]
+
+
+# In[4]:
+
+
+dff
+
+
+# In[5]:
+
+
+dff2= dff.describe()
+
+
+# In[6]:
+
+
+dff2
+
+
+# In[7]:
+
+
+dff3= dff.mode()
+
+
+# In[8]:
+
+
+dff3
+
+
+# In[ ]:
+
+
+
+
